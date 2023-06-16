9fcf7c455b1f67b58cb533a8d388244c5861a341
mc3Kast <k3sha2014@yandex.ru>
2023-06-16 12:19:28 +0300

    feat: add some routing

---
+++ Backend/Controllers/OfferContoller.cs
@@ -0,0 +1,80 @@
+﻿using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Backend.Controllers
+{
+    public class OfferController : Controller
+    {
+        private readonly ILogger<OfferController> _logger;
+
+        public OfferController(ILogger<OfferController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Resedentials()
+        {
+            return View();
+        }
+
+        [Route("Offer/Resedentials/Leasing")]
+        public IActionResult ResLeasing()
+        {
+            return View();
+        }
+
+        [Route("Offer/Resedentials/Leasing/flat")]
+        public IActionResult FlaLeasing()
+        {
+            return View();
+        }
+
+        [Route("Offer/Resedentials/Leasing/room")]
+        public IActionResult RomLeasing()
+        {
+            return View();
+        }
+
+        [Route("Offer/Resedentials/Leasing/house")]
+        public IActionResult HosLeasing()
+        {
+            return View();
+        }
+
+        [Route("Offer/Resedentials/Sale")]
+        public IActionResult ResSale()
+        {
+            return View();
+        }
+
+        [Route("Offer/Resedentials/sale/flat")]
+        public IActionResult FlaSale()
+        {
+            return View();
+        }
+
+        [Route("Offer/Resedentials/sale/room")]
+        public IActionResult RomSale()
+        {
+            return View();
+        }
+
+        [Route("Offer/Resedentials/sale/house")]
+        public IActionResult HosSale()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
