8b16bfaac481b4ded83365a642e392f51d94e49f
Alexander <m.kononv.alwxander@gmail.com>
2023-06-18 18:22:44 +0300

    Severe refactor of RealtService
    Now it follows Clean Architecture:
    	Core is composed of Domain and Application.
    	Domain describes Enterprice Logic and Application describes Business Logic.
    	Presentation Layer provide the API to clients.
    	Infrastructure Layer deals with External Components. At this moment
    	Database and Identity Server are provided for. List is not final.

---
+++ Domain/Entities/Class2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealtService.Domain.Entities;
+
+public class Class2
+{
+}
