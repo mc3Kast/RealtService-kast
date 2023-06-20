b288d30cd9f64794560e46b2c9bbe36f2fd46944
mc3Kast <k3sha2014@yandex.ru>
2023-06-20 22:46:00 +0300

    feat: add  a little DI

---
+++ Persistence/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RealtService.Application.Interfaces;
+
+namespace RealtService.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<RealtServiceDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+            services.AddScoped<IOfferDbContext>(provider =>
+            
+                provider.GetService<RealtServiceDbContext>()
+            );
+            return services;
+        }
+    }
+}
