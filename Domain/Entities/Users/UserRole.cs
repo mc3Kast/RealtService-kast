6fc72424eca327a70bca97d35b66711d0e459645
Alexander <m.kononv.alwxander@gmail.com>
2023-06-29 06:00:51 +0300

    Merged with my stash

--- Domain/Entities/Users/UserRole.cs
+++ Domain/Entities/Users/UserRole.cs
@@ -6,13 +6,11 @@ public class UserRole : NamedEntity
 {
     public static readonly UserRole USER = new UserRole() 
     { 
-        Id = 1,
         Name = "USER"
     };
 
     public static readonly UserRole ADMIN = new UserRole()
     {
-        Id = 2,
         Name = "ADMIN"
     };
 
